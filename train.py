7b34cdb1bb6de843360ce4ba2b30da860bb62d26
Yiling Chen <balling.xo@gmail.com>
2019-12-04 10:00:59 -0800

    use large training files

--- train.py
+++ train.py
@@ -10,15 +10,15 @@ import trainer
 
 
 if __name__ == "__main__":
-    # train_lookup_path = os.path.join(DATA_DIR, 'lookups-tiny.pickle')
-    # train_user_path = os.path.join(DATA_DIR, 'users-tiny.pickle')
-    train_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
-    train_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
+    train_lookup_path = os.path.join(DATA_DIR, 'lookups-tiny.pickle')
+    train_user_path = os.path.join(DATA_DIR, 'users-tiny.pickle')
+    # train_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
+    # train_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
     val_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
     val_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
-    # test_lookup_path = os.path.join(DATA_DIR, 'lookups-test.pickle')
-    # test_user_path = os.path.join(DATA_DIR, 'users-test.pickle')
-    test_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
-    test_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
+    test_lookup_path = os.path.join(DATA_DIR, 'lookups-test.pickle')
+    test_user_path = os.path.join(DATA_DIR, 'users-test.pickle')
+    # test_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
+    # test_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
 
     trainer.train_pipeline(PlainRNN, train_lookup_path, train_user_path, val_lookup_path, val_user_path, test_lookup_path, test_user_path, TRAINING_PARAMS)
