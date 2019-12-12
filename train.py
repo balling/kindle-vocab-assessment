e3f9da461eaa723bad9220d5b5c71cc8345e1c59
Yiling Chen <balling.xo@gmail.com>
2019-12-12 13:42:13 -0800

    update data path

--- train.py
+++ train.py
@@ -3,21 +3,21 @@
 import os
 import pickle
 
-from models import PlainRNN
+from models import PlainRNN, LinearModel
 from config import TRAINING_PARAMS, DATA_DIR
 
 import trainer
 
 
 if __name__ == "__main__":
-    train_lookup_path = os.path.join(DATA_DIR, 'lookups-tiny.pickle')
-    train_user_path = os.path.join(DATA_DIR, 'users-tiny.pickle')
+    train_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
+    train_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
     # train_lookup_path = os.path.join(DATA_DIR, 'lookups-test.pickle')
     # train_user_path = os.path.join(DATA_DIR, 'users-test.pickle')
     val_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
     val_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
-    test_lookup_path = os.path.join(DATA_DIR, 'lookups-test.pickle')
-    test_user_path = os.path.join(DATA_DIR, 'users-test.pickle')
+    test_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
+    test_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
     # test_lookup_path = os.path.join(DATA_DIR, 'lookups-teeny.pickle')
     # test_user_path = os.path.join(DATA_DIR, 'users-teeny.pickle')
 
